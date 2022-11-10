f9b1905255a89f4170c7fb500d054a32dd4ae1f5
minkyoung kim <minry20415@naver.com>
2022-11-10 11:08:10 +0900

    springboot MVC를 이용하여 게시판 구현하기 1. add (새 게시글 작성) 2. findById (작성한 게시글 번호 찾아서 보기) 3. list (작성한 모든 게시글 목록보기) 4. update (작성한 게시글 수정하기) 5. delete (작성한 게시글 삭제하기)

---
+++ src/main/java/com/mustache/bbs4/SpringbootMustache4Application.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustache4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMustache4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustache/bbs4/SpringbootMustache4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustache4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
